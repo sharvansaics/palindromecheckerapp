6ff95e31b548198471ef84ce7e747c4573eb50e5
Amrit Roushan <ar5453@srmist.edu.in>
2026-02-23 09:40:08 +0530

    uc2 completed

--- Palindromecheckerapp/src/Palindromecheckerapp.java
+++ Palindromecheckerapp/src/Palindromecheckerapp.java
@@ -1,7 +1,15 @@
 public class Palindromecheckerapp {
-    public static void main(string[] args){
-        System.out.println("welcome to palindrome");
-        System.out.println("version 1.0");
-        System.out.println("system initialize");
+    public static void main(String[] args) {
+        String original = "radar";
+        String reverse = "";
+
+        // Using StringBuilder to reverse the string easily
+        reverse = new StringBuilder(original).reverse().toString();
+
+        if (original.equals(reverse)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
     }
 }
