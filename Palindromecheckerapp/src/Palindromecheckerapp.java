14fb4e688e4e71bd52fbacf309631c00b83304b7
Amrit Roushan <ar5453@srmist.edu.in>
2026-02-23 10:14:20 +0530

    uc6 completed

--- Palindromecheckerapp/src/Palindromecheckerapp.java
+++ Palindromecheckerapp/src/Palindromecheckerapp.java
@@ -3,7 +3,7 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class PalindromeComparison {
+public class Palindromecheckerapp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
