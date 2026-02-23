bd9f33eaf59091714799257ff365ab232538a715
Amrit Roushan <ar5453@srmist.edu.in>
2026-02-23 09:25:47 +0530

    initial commit

---
+++ Palindromecheckerapp/src/Palindromecheckerapp.java
@@ -0,0 +1,7 @@
+public class Palindromecheckerapp {
+    public static void main(string[] args){
+        System.out.println("welcome to palindrome");
+        System.out.println("version 1.0");
+        System.out.println("system initialize");
+    }
+}
