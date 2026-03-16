26efe98472b6135597eea530ecf81bdb90ab7a68
sharvansaics <sc1781@srmist.edu.in>
2026-03-16 18:53:05 +0530

    UC8

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,39 +1,71 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
-
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        if (isPalindrome(input)) {
-            System.out.println("\"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("\"" + input + "\" is not a palindrome.");
+class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
-        scanner.close();
     }
-
-    public static boolean isPalindrome(String str) {
-        if (str == null) {
-            return false;
+    static Node head = null;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        input = input.replaceAll("\\s+", "").toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            append(input.charAt(i));
         }
-
-        String cleaned = str.toLowerCase();
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < cleaned.length(); i++) {
-            deque.addLast(cleaned.charAt(i));
+        if (isPalindrome(head)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is not a Palindrome.");
         }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
+    }
+    static void append(char data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
                 return false;
             }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
         }
-
         return true;
+    }
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
     }
 }
